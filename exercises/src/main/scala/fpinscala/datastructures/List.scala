845998f504027af9a3aafe222dab5dbce1af3585
Mariano Navas <marianudo@gmail.com>
2015-05-28 00:50:34 +0200

    doubleToString in List

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -121,5 +121,8 @@ object List { // `List` companion object. Contains functions for creating and wo
   def add1(l: List[Int]): List[Int] =
     foldRight(l, Nil: List[Int])((a, b) => Cons(a + 1, b))
 
+  def doubleToString(l: List[Double]): List[String] =
+    foldRight(l, Nil: List[String])((a, b) => Cons(a.toString, b))
+
   def map[A,B](l: List[A])(f: A => B): List[B] = sys.error("todo")
 }
