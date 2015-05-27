74a50f615c62ef8aa60919bac975beddca46b73a
Mariano Navas <marianonavas@packlink.es>
2015-05-27 12:12:59 +0200

    Implement head function on list

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -55,7 +55,10 @@ object List { // `List` companion object. Contains functions for creating and wo
     case Cons(x, xs) => xs
   }
 
-  def setHead[A](l: List[A], h: A): List[A] = sys.error("todo")
+  def setHead[A](l: List[A], h: A): List[A] = l match {
+    case Nil => Cons(h, Nil)
+    case Cons(x, xs) => Cons(h, xs)
+  }
 
   def drop[A](l: List[A], n: Int): List[A] = sys.error("todo")
 
