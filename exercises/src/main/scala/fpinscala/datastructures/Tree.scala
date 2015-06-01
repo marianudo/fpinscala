01f5a6031f41ed8aecdd1ad8b824eb91ac77c028
Mariano Navas <marianudo@gmail.com>
2015-06-01 16:45:30 +0200

    Size function in Tree
    From the top of my head

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -6,8 +6,11 @@ case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
 
 object Tree {
-
-
-
-
+  def size[A](t: Tree[A]): Int = {
+    def f(t: Tree[A], acc: Int): Int = t match {
+      case Leaf(_) => acc + 1
+      case Branch(l, r) => f(l, acc) + f(r, acc)
+    }
+    f(t, 0)
+  }
 }
