c9a1183a0311947c7c4e73b4a80943d3f0768d95
Mariano Navas <marianudo@gmail.com>
2015-06-01 11:07:48 +0200

    Define filter function

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -131,4 +131,8 @@ object List { // `List` companion object. Contains functions for creating and wo
 
   def map[A,B](l: List[A])(f: A => B): List[B] =
     foldRight(l, Nil: List[B])((a, b) => Cons(f(a), b))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, Nil: List[A])((a, b) => if(f(a)) Cons(a, b) else b)
+
 }
