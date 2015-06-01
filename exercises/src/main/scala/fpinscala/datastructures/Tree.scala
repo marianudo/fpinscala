01c9096566f73616783af4e5faaed580b4835fa7
Mariano Navas <marianudo@gmail.com>
2015-06-01 17:19:32 +0200

    Depth function in Tree

--- exercises/src/main/scala/fpinscala/datastructures/Tree.scala
+++ exercises/src/main/scala/fpinscala/datastructures/Tree.scala
@@ -15,4 +15,9 @@ object Tree {
     case Leaf(v) => v
     case Branch(l, r) => maximum(l) max maximum(r)
   }
+
+  def depth[A](t: Tree[A]): Int = t match {
+    case Leaf(_) => 0
+    case Branch(l, r) => 1 + (depth(l) max depth(r))
+  }
 }
