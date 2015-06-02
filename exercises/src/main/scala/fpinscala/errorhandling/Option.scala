b4bf7551a611494eb9f12d630d96b98484fff87e
Mariano Navas <marianudo@gmail.com>
2015-06-02 09:53:08 +0200

    Fixes in Option functions

--- exercises/src/main/scala/fpinscala/errorhandling/Option.scala
+++ exercises/src/main/scala/fpinscala/errorhandling/Option.scala
@@ -14,11 +14,14 @@ sealed trait Option[+A] {
     case Some(x) => x
   }
 
-  def flatMap[B](f: A => Option[B]): Option[B] = map(f).getOrElse(None)
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f).getOrElse(None)
 
-  def orElse[B>:A](ob: => Option[B]): Option[B] = ob
+  def orElse[B>:A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
 
-  def filter(f: A => Boolean): Option[A] = if (f(this)) Some(A) else None
+  def filter(f: A => Boolean): Option[A] =
+    flatMap(a => if (f(a)) Some(a) else None)
 }
 case class Some[+A](get: A) extends Option[A]
 case object None extends Option[Nothing]
