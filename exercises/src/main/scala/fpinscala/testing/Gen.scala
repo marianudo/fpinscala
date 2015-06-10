03bad144b384cd3d7bdcb5ef5300d6003d175543
Mariano Navas <marianudo@gmail.com>
2015-06-10 16:23:29 +0200

    Some more non-primitive compositors for Gen

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -47,8 +47,18 @@ object Gen {
 
 case class Gen[A](sample: State[RNG,A]) {
   def map[A,B](f: A => B): Gen[B] = ???
+
   def flatMap[B](f: A => Gen[B]): Gen[B] =
     Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN(size: Int): Gen[List[A]] =
+    Gen.listOfN(size, this)
+
+  def listOfN(size: Gen[Int]): Gen[List[A]] =
+   size flatMap (n => this.listOfN(n))
+
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap(b => if (b) g1 else g2)
 }
 
 trait SGen[+A] {
