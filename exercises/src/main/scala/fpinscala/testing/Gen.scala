f423cebb6ed9e69fe428ccf560a46487ecded8e6
Mariano Navas <marianudo@gmail.com>
2015-06-10 11:56:50 +0200

    choose function in property based testing chapter

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -26,9 +26,17 @@ object Prop {
 
 object Gen {
   def unit[A](a: => A): Gen[A] = ???
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+    Gen(State(RNG.nonNegativeInt) map (n => n + start % (stopExclusive - start)))
+    // Alternative, baby steps based implementation (this is the one it's easier for me to come up with on my own)
+    //val positiveIntState = State(RNG.nonNegativeInt)
+    //val inRangeState = positiveIntState map (n => n + start % (stopExclusive - n))
+    //Gen(inRangeState)
+  }
 }
 
-trait Gen[A] {
+case class Gen[A](sample: State[RNG,A]) {
   def map[A,B](f: A => B): Gen[B] = ???
   def flatMap[A,B](f: A => Gen[B]): Gen[B] = ???
 }
